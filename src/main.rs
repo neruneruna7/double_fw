a9c30616052a19e639bbb39b67af6b312e5b72bd
neruneruna7 <neruneruna7@gmail.com>
2024-05-03 10:13:26 +0900

    fmt

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use actix_web::{web::{self, block}, App, HttpRequest, HttpServer};
+use actix_web::{
+    web::{self, block},
+    App, HttpRequest, HttpServer,
+};
 use tracing_actix_web::TracingLogger;
 
 // #[instrument]
@@ -7,65 +10,48 @@ async fn index(req: HttpRequest) -> &'static str {
     "Hello world!"
 }
 
-#[tokio::main]
-async fn main() -> std::io::Result<()> {
-    body().await;
-
-
-    HttpServer::new(|| {
-        App::new()
-            // enable logger
-            .service(web::resource("/index.html").to(|| async { "Hello world!" }))
-            .service(web::resource("/").to(index))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+async fn dopanic() -> &'static str {
+    println!("dopanic");
+    panic!("dopanic");
+    "dopanic"
 }
 
-fn te(){
-    tokio::runtime::Builder::new_multi_thread()
-        .enable_all()
-        .build()
-        .expect("Failed building the Runtime")
-        .block_on(async {});
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    body().await
 }
 
 async fn body() -> std::io::Result<()> {
     // 2つの非同期を実行し，joinでまつ
-    let (a, b) = tokio::join!(async {
-        // 1秒待つ
-        actix_body1().await
-    }, async {
-        // 2秒待つ
-        actix_body2().await
-    });
+    let (a, b) = tokio::join!(actix_body1(), actix_body2());
 
     a?;
     b?;
     Ok(())
 }
 
-async fn actix_body1() ->  std::io::Result<()> {
+async fn actix_body1() -> std::io::Result<()> {
     println!("actix_body1");
     HttpServer::new(|| {
         App::new()
             // enable logger
             .service(web::resource("/index.html").to(|| async { "Hello world!" }))
             .service(web::resource("/").to(index))
+            .service(web::resource("/dopanic").to(dopanic))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
 }
 
-async fn actix_body2() ->  std::io::Result<()> {
-    println!("actix_body1");
+async fn actix_body2() -> std::io::Result<()> {
+    println!("actix_body2");
     HttpServer::new(|| {
         App::new()
             // enable logger
             .service(web::resource("/index.html").to(|| async { "Hello world!" }))
             .service(web::resource("/").to(index))
+            .service(web::resource("/dopanic").to(dopanic))
     })
     .bind(("127.0.0.1", 8081))?
     .run()
